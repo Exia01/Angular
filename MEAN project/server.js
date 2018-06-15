a54b0c1283fdf92eaed1219eb45391e920c2a1c0
Jose <sixgg4@gmail.com>
2018-06-15 16:55:04 -0400

    Initial Commit

--- MEAN project/server.js
+++ MEAN project/server.js
@@ -13,7 +13,6 @@ app.use(bodyParser.urlencoded({encoded: true}))// Use body parser
 app.use(express.static(path.join(__dirname, './client/static')))// Identify static folder
 app.use(session({secret: 'SECRET_KEY'}))//  Make your own secret key. Don't be lazy
 app.set('views', path.join(__dirname, './client/views'))// Identify views folders
-app.set('view engine', 'ejs')// Set EJS as the view engine
 
 require('./server/config/routes.js')(app)
 require('./server/config/mongoose.js')
